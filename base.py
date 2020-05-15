d20aa2ee769c4c6663d7ac3243dfb689e30e5563
Luis García Díaz <zethes@gmail.com>
2020-05-15 14:36:58 -0500

    Update base.py

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def conexion():
     mydb=mysql.connector.connect(
-    host="172.30.27.40",
+    host="localhost",
     user="root",
     passwd="",
     database="base_cordi"
